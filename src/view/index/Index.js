eb501ce25c20af0c7c7984fc949384f06dabbf7f
NowUI <nowui@163.com>
2018-01-15 01:35:59 +0800

    统一前端布局写法

--- src/view/index/Index.js
+++ src/view/index/Index.js
@@ -10,48 +10,94 @@ class Index extends Component {
         super(props);
 
         this.state = {
-
+            opacity: 0
         }
     }
 
     componentDidMount() {
         util.setTitle('wawipet哇咿宠');
+
+        window.addEventListener('scroll', this.handleScroll.bind(this));
+
+        if (util.isIE8()) {
+            setTimeout(function () {
+                this.handleHeader();
+            }.bind(this), 10);
+
+            window.addEventListener('scroll', this.handleScrollHeader.bind(this));
+        }
+
+        new window.Swiper('.swiper-container', {
+            pagination: '.pagination',
+            loop: true
+        })
     }
 
     componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll.bind(this));
 
+        if (util.isIE8()) {
+            window.removeEventListener('scroll', this.handleScrollHeader.bind(this));
+        }
+    }
+
+    handleScroll() {
+        let scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+
+        this.setState({
+            opacity: scrollTop / 300
+        });
+    }
+
+    handleScrollHeader() {
+        this.handleHeader();
+    }
+
+    handleHeader() {
+        util.scrollToFixed('index-header', 0);
     }
 
     render() {
+        const settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            accessibility: false,
+            variableWidth: true
+        };
+
         return (
             <div>
+                <div className="index-header-mask" style={{
+                    opacity: this.state.opacity,
+                    filter: 'progid:DXImageTransform.Microsoft.Alpha(opacity=100)'
+                }}></div>
                 <div className="index-header">
                     <div className="index-header-left">
-                        <img className="index-header-left-user" src={require('../../image/index-header-left-user.png')} alt=""/>
+                        <img className="index-header-left-user" src={require('../../image/index-header-left-user.png')}
+                             alt=""/>
                     </div>
                     <div className="index-header-center">
-                        <img className="index-header-center-logo" src={require('../../image/index-header-center-logo.png')} alt=""/>
+                        <img className="index-header-center-logo"
+                             src={require('../../image/index-header-center-logo.png')} alt=""/>
                     </div>
                     <div className="index-header-right">
-                        <img className="index-header-right-search" src={require('../../image/index-header-right-search.png')} alt=""/>
+                        <img className="index-header-right-search"
+                             src={require('../../image/index-header-right-search.png')} alt=""/>
                     </div>
                 </div>
-                <div className="container">
-                    <div>1</div>
-                    <div>1</div>
-                    <div>1</div>
-                    <div>1</div>
-                    <div>1</div>
-                    <div>1</div>
+                <div className="index-banner swiper-container">
+                    <div className="swiper-wrapper"
+                         style={{height: (document.documentElement.clientWidth / 320 * 380) + 'px'}}>
+                        <div className="swiper-slide">
+                            <img className="index-banner-image" src={require('../../image/0.png')} alt=""/>
+                        </div>
+                        <div className="swiper-slide">
+                            <img className="index-banner-image" src={require('../../image/0.png')} alt=""/>
+                        </div>
+                    </div>
+                    <div className="pagination"></div>
                 </div>
-                <div className="bottom-line">1</div>
-                <div className="bottom-line">1</div>
-                <div className="bottom-line">1</div>
-                <div className="bottom-line">1</div>
-                <div className="bottom-line">1</div>
-                <div className="bottom-line">1</div>
-                <div className="bottom-line">1</div>
-                <div className="bottom-line">1</div>
                 <br/>
                 <br/>
                 <br/>
