f76500bd2024f29bf49933a1c7458eaa76b93ea4
钟永豪 <zhy6421867@qq.com>
2018-01-14 18:08:11 +0800

    finish layout
    finish layout

--- src/view/forum/Info.js
+++ src/view/forum/Info.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux';
 
 import util from '../../common/util';
 
-import './Index.css';
+import './Info.css';
 
 class Index extends Component {
     constructor(props) {
@@ -25,7 +25,123 @@ class Index extends Component {
     render() {
         return (
             <div>
-
+                <div className="forum-info-header">
+                    圈子信息
+                </div>
+                <div className="forum-info-avatar">
+                    <div className="forum-info-title">
+                        圈子头像
+                    </div>
+                    <div className="forum-info-upload">
+                        <img src={require('../../image/avatar-demo.png')} alt=""/>
+                    </div>
+                    <div className="forum-info-right">
+                        <div></div>
+                    </div>
+                </div>
+                <div className="forum-info-box">
+                    <div className="forum-info-item">
+                        <div className="forum-info-title">
+                            圈子名称
+                        </div>
+                        <div className="forum-info-text">
+                            魔都喵星人
+                        </div>
+                        <div className="forum-info-right">
+                            <div></div>
+                        </div>
+                    </div>
+                    <div className="forum-info-item">
+                        <div className="forum-info-title">
+                            圈子简介
+                        </div>
+                        <div className="forum-info-text">
+                            这里是魔都喵星人的聚集地
+                        </div>
+                        <div className="forum-info-right">
+                            <div></div>
+                        </div>
+                    </div>
+                    <div className="forum-info-panel">
+                        <div className="forum-info-title">
+                            圈子简介
+                        </div>
+                        <div className="forum-info-owner-box">
+                            <div className="forum-info-owner-avatar">
+                                <img src={require('../../image/header-demo.png')} alt=""/>
+                                <div className="forum-info-owner-crown">
+                                    <img src={require('../../image/icon-crown.png')} alt=""/>
+                                </div>
+                            </div>
+                            <div className="forum-info-owner-info">
+                                <div className="forum-info-owner-name">
+                                    小野
+                                </div>
+                                <div className="forum-info-owner-des">
+                                    资深遛狗师一枚，对宠物行为有很深的造诣。
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="forum-info-panel">
+                        <div className="forum-info-title">
+                            全部圈友
+                        </div>
+                        <div className="forum-info-list">
+                            <ul>
+                                <li>
+                                    <img src={require('../../image/header-demo.png')} alt=""/>
+                                </li>
+                                <li>
+                                    <img src={require('../../image/header-demo.png')} alt=""/>
+                                </li>
+                                <li>
+                                    <img src={require('../../image/header-demo.png')} alt=""/>
+                                </li>
+                                <li>
+                                    <img src={require('../../image/header-demo.png')} alt=""/>
+                                </li>
+                                <li>
+                                    <img src={require('../../image/header-demo.png')} alt=""/>
+                                </li>
+                                <li>
+                                    <img src={require('../../image/header-demo.png')} alt=""/>
+                                </li>
+                                <li>
+                                    <img src={require('../../image/header-demo.png')} alt=""/>
+                                </li>
+                            </ul>
+                            <ul>
+                                <li>
+                                    <img src={require('../../image/header-demo.png')} alt=""/>
+                                </li>
+                                <li>
+                                    <img src={require('../../image/header-demo.png')} alt=""/>
+                                </li>
+                                <li>
+                                    <img src={require('../../image/header-demo.png')} alt=""/>
+                                </li>
+                                <li>
+                                    <img src={require('../../image/header-demo.png')} alt=""/>
+                                </li>
+                                <li>
+                                    <img src={require('../../image/header-demo.png')} alt=""/>
+                                </li>
+                                <li>
+                                    <img src={require('../../image/header-demo.png')} alt=""/>
+                                </li>
+                                <li>
+                                    <img src={require('../../image/header-demo.png')} alt=""/>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="forum-info-delete-btn">
+                        <button>
+                            删除圈子
+                        </button>
+                    </div>
+                </div>
             </div>
         );
     }
