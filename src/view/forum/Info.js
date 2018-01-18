54f7b2697dd9924544a3a94f40705d77c9acb01d
NowUI <nowui@163.com>
2018-01-18 17:17:52 +0800

    增加圈子信息页面

--- src/view/forum/Info.js
+++ src/view/forum/Info.js
@@ -37,99 +37,101 @@ class Info extends Component {
                         圈子信息
                     </div>
                 </div>
-                <div className={style.image}>
-                    <div className={style.imageLeft}>上传圈子照片</div>
-                    <div className={style.imageCenter}>
-                        <img className={style.imageCenterImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/58/h/58' alt=''/>
-                    </div>
-                    <div className={style.imageRight}>
-                        <div className={baseStyle.rightArrow}></div>
+                <div className={style.content}>
+                    <div className={style.image}>
+                        <div className={style.imageLeft}>上传圈子照片</div>
+                        <div className={style.imageCenter}>
+                            <img className={style.imageCenterImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/58/h/58' alt=''/>
+                        </div>
+                        <div className={style.imageRight}>
+                            <div className={baseStyle.rightArrow}></div>
+                        </div>
                     </div>
                 </div>
                 <div className={style.line}></div>
-                <div className={classNames(baseStyle.list, baseStyle.bottomLine)}>
-                    <div className={baseStyle.listLeft}>圈子名称</div>
-                    <div className={classNames(style.listCenter, baseStyle.listCenter)}>
-                        魔都喵星人
-                    </div>
-                    <div className={baseStyle.listRight}>
-                        <div className={baseStyle.rightArrow}></div>
-                    </div>
-                </div>
-                <div className={classNames(baseStyle.list, baseStyle.bottomLine)}>
-                    <div className={baseStyle.listLeft}>圈子简介</div>
-                    <div className={classNames(style.listCenter, baseStyle.listCenter)}>
-                        这里是魔都喵星人的聚集这里是魔都喵星人的聚集这里是魔都喵星人的聚集
-                    </div>
-                    <div className={baseStyle.listRight}>
-                        <div className={baseStyle.rightArrow}></div>
-                    </div>
-                </div>
-                <div className={classNames(baseStyle.list, baseStyle.bottomLine)}>
-                    <div className={style.infoTitle}>
-                        圈子信息
-                    </div>
-                    <div className={style.info}>
-                        <div className={style.infoLeft}>
-                            <img className={style.infoLeftCrown}
-                                 src={require('../../image/crown.png')}
-                                 alt=''/>
-                            <img className={style.infoLeftImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/35/h/35' alt=''/>
+                <div className={style.content}>
+                    <div className={classNames(baseStyle.list, baseStyle.bottomLine)}>
+                        <div className={baseStyle.listLeft}>圈子名称</div>
+                        <div className={classNames(style.listCenter, baseStyle.listCenter)}>
+                            魔都喵星人
                         </div>
-                        <div className={style.infoRight}>
-                            <div className={style.infoRightName}>小野</div>
-                            <div className={style.infoRightDescription}>资深遛狗师一枚，对宠物行为有很深的造诣。资深遛狗师一枚，对宠物行为有很深的造诣。资深遛狗师一枚，对宠物行为有很深的造诣。</div>
+                        <div className={baseStyle.listRight}>
+                            <div className={baseStyle.rightArrow}></div>
                         </div>
                     </div>
-                </div>
-                <div className={baseStyle.list}>
-                    <div className={style.memberTitle}>
-                        全部圈友
-                    </div>
-                    <div className={style.member}>
-                        <div className={style.memberAvatar}>
-                            <img className={style.memberAvatarImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/35/h/35' alt=''/>
+                    <div className={classNames(baseStyle.list, baseStyle.bottomLine)}>
+                        <div className={baseStyle.listLeft}>圈子简介</div>
+                        <div className={classNames(style.listCenter, baseStyle.listCenter)}>
+                            这里是魔都喵星人的聚集这里是魔都喵星人的聚集这里是魔都喵星人的聚集
                         </div>
-                        <div className={style.memberAvatar}>
-                            <img className={style.memberAvatarImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/35/h/35' alt=''/>
-                        </div>
-                        <div className={style.memberAvatar}>
-                            <img className={style.memberAvatarImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/35/h/35' alt=''/>
-                        </div>
-                        <div className={style.memberAvatar}>
-                            <img className={style.memberAvatarImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/35/h/35' alt=''/>
-                        </div>
-                        <div className={style.memberAvatar}>
-                            <img className={style.memberAvatarImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/35/h/35' alt=''/>
-                        </div>
-                        <div className={style.memberAvatar}>
-                            <img className={style.memberAvatarImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/35/h/35' alt=''/>
-                        </div>
-                        <div className={style.memberAvatar}>
-                            <img className={style.memberAvatarImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/35/h/35' alt=''/>
-                        </div>
-                        <div className={style.memberAvatar}>
-                            <img className={style.memberAvatarImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/35/h/35' alt=''/>
-                        </div>
-                        <div className={style.memberAvatar}>
-                            <img className={style.memberAvatarImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/35/h/35' alt=''/>
-                        </div>
-                        <div className={style.memberAvatar}>
-                            <img className={style.memberAvatarImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/35/h/35' alt=''/>
-                        </div>
-                        <div className={style.memberAvatar}>
-                            <img className={style.memberAvatarImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/35/h/35' alt=''/>
-                        </div>
-                        <div className={style.memberAvatar}>
-                            <img className={style.memberAvatarImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/35/h/35' alt=''/>
-                        </div>
-                        <div className={style.memberAvatar}>
-                            <img className={style.memberAvatarImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/35/h/35' alt=''/>
+                        <div className={baseStyle.listRight}>
+                            <div className={baseStyle.rightArrow}></div>
                         </div>
                     </div>
-                </div>
-                <div className={style.delete}>
-                    <div className={style.deleteButton}>删除圈子</div>
+                    <div className={classNames(baseStyle.list, baseStyle.bottomLine)}>
+                        <div className={style.infoTitle}>
+                            圈子信息
+                        </div>
+                        <div className={style.info}>
+                            <div className={style.infoLeft}>
+                                <img className={style.infoLeftCrown}
+                                     src={require('../../image/crown.png')}
+                                     alt=''/>
+                                <img className={style.infoLeftImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/35/h/35' alt=''/>
+                            </div>
+                            <div className={style.infoRight}>
+                                <div className={style.infoRightName}>小野</div>
+                                <div className={style.infoRightDescription}>资深遛狗师一枚，对宠物行为有很深的造诣。资深遛狗师一枚，对宠物行为有很深的造诣。资深遛狗师一枚，对宠物行为有很深的造诣。</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={baseStyle.list}>
+                        <div className={style.memberTitle}>
+                            全部圈友
+                        </div>
+                        <div className={style.member}>
+                            <div className={style.memberAvatar}>
+                                <img className={style.memberAvatarImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/35/h/35' alt=''/>
+                            </div>
+                            <div className={style.memberAvatar}>
+                                <img className={style.memberAvatarImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/35/h/35' alt=''/>
+                            </div>
+                            <div className={style.memberAvatar}>
+                                <img className={style.memberAvatarImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/35/h/35' alt=''/>
+                            </div>
+                            <div className={style.memberAvatar}>
+                                <img className={style.memberAvatarImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/35/h/35' alt=''/>
+                            </div>
+                            <div className={style.memberAvatar}>
+                                <img className={style.memberAvatarImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/35/h/35' alt=''/>
+                            </div>
+                            <div className={style.memberAvatar}>
+                                <img className={style.memberAvatarImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/35/h/35' alt=''/>
+                            </div>
+                            <div className={style.memberAvatar}>
+                                <img className={style.memberAvatarImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/35/h/35' alt=''/>
+                            </div>
+                            <div className={style.memberAvatar}>
+                                <img className={style.memberAvatarImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/35/h/35' alt=''/>
+                            </div>
+                            <div className={style.memberAvatar}>
+                                <img className={style.memberAvatarImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/35/h/35' alt=''/>
+                            </div>
+                            <div className={style.memberAvatar}>
+                                <img className={style.memberAvatarImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/35/h/35' alt=''/>
+                            </div>
+                            <div className={style.memberAvatar}>
+                                <img className={style.memberAvatarImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/35/h/35' alt=''/>
+                            </div>
+                            <div className={style.memberAvatar}>
+                                <img className={style.memberAvatarImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/35/h/35' alt=''/>
+                            </div>
+                            <div className={style.memberAvatar}>
+                                <img className={style.memberAvatarImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/35/h/35' alt=''/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={style.delete}>删除圈子</div>
                 </div>
             </div>
         );
