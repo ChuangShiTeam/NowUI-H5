54a584fc629aa1640ffcd974495ec8a939e2a9b4
xupengfei111 <15303933327@163.com>
2018-01-22 14:09:19 +0800

    动态详情页

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './Router';
-
 import './css/Style.css';
 import 'rc-notification/assets/index.css';
 import 'rmc-select-list/assets/index.css';
