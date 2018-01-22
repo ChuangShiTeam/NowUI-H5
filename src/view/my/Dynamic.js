5324815723ecc085d72b2996ab0675b16514cd58
ZHB <zhb020@gmail.com>
2018-01-22 10:00:42 +0800

    收藏界面文章(bookmark,兼容)

---
+++ src/view/my/Dynamic.js
@@ -0,0 +1,99 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+import util from '../../common/util';
+
+import style from './Dynamic.scss';
+class Dynamic extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isLoad: false
+        }
+    }
+
+    componentDidMount() {
+        util.setTitle('wawipet哇咿宠');
+
+    }
+
+    componentWillUnmount() {
+
+    }
+    render() {
+        return (
+            <div className={style.page}>
+                <div className={style.navList}>
+                    <div className={style.navList1}>全部</div>
+                    <div className={style.navList2}>文章</div>
+                    <div className={style.running}>动态</div>
+                    <div className={style.navList2}>萌物</div>
+                    <div className={style.navList2}>商户</div>
+                    <div className={style.search}>
+                        <img src={require("../../image/search.png")} alt=""/>
+                    </div>
+                </div>
+                <div className={style.content}>
+                    <div>
+                        <div className={style.contentList}>
+                            <div className={style.contentLeft}>
+                                
+                                <div className={style.contentLeftAll}>
+                                    <div className={style.contentLeft1}>
+                                    
+                                        <img src="http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/68/h/68" alt=""/>
+                                        <div className={style.contentLeftFoot}>
+                                            <div className={style.contentLeftFoot1}> 为工作而生的汪星人拉布...</div>
+                                            <div className={style.contentLeftFoot3}>
+                                                <div>收藏于:2018-1-10 16.:44</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div></div>
+                                        
+                                </div>
+
+
+
+
+
+
+
+
+                            </div>
+                            <div className={style.contentRight}>
+                                <div>
+                                    <img src={require("../../image/star.png")} alt=""/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className={style.contentList}>
+                            <div className={style.contentLeft}>
+                                <div className={style.contentLeft1}>
+                                    <img src="http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/68/h/68" alt=""/>
+                                    <div className={style.contentLeftFoot}>
+                                        <div className={style.contentLeftFoot1}> 什么？现在宠物的专机如…</div>
+                                        <div className={style.contentLeftFoot3}>
+                                            <div>收藏于:2017-12-01 12:02</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={style.contentRight}>
+                                <div>
+                                    <img src={require("../../image/star.png")} alt=""/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+            ;
+    }
+}
+export default connect(() => ({}))(Dynamic);
