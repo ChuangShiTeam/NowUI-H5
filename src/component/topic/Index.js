c94ff6590f179530c4d1b40dd6a7e74216a0a6ef
marcus <marcus_hualebin@163.com>
2018-01-22 21:18:32 +0800

    话题主页完善

--- src/component/topic/Index.js
+++ src/component/topic/Index.js
@@ -1,7 +1,10 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import {Link} from 'react-router';
 import classNames from 'classnames';
+import moment from 'moment';
 
+import constant from '../../common/constant';
 import style from './Index.scss';
 import baseStyle from '../../css/Base.scss';
 
@@ -25,59 +28,78 @@ class Index extends Component {
     render() {
         return (
             <div className={baseStyle.page}>
-                <div className={style.header}>
-                    <div className={style.headerLeft}>
-                        <img className={style.headerLeftImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/28/h/28' alt=''/>
-                    </div>
-                    <div className={style.headerCenter}>
-                        <p className={style.headerCenterName}>Nami</p>
-                        <p className={style.headerCenterTime}>20分钟前</p>
-                    </div>
-                    <div className={style.headerRight}></div>
-                </div>
-                <Link to="/topic/detail" className={style.content}>
-                    <img className={style.contentImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/320/h/255' alt=''/>
-                </Link>
-                <div className={style.footer}>
-                    <div className={classNames(style.footerText, baseStyle.bottomLine)}>
-                        奶糖和福禄娃给你过圣诞啦～
-                    </div>
-                    <div className={style.footerInfo}>
-                        <div className={style.footerInfoCount}>
-                            <div className={style.footerInfoCountLike}>
-                                <img className={style.footerInfoCountLikeIcon} src={true ? require('../../image/like.png') : require('../../image/like-active.png')} alt=''/>
-                                <span className={style.footerInfoCountLikeNumber}>10</span>
+                {
+                    this.props.topic && this.props.topic.topicId ?
+                        <div>
+                            <div className={style.header}>
+                                <div className={style.headerLeft}>
+                                    {
+                                        this.props.topic.userAvatar ?
+                                            <img className={style.headerLeftImage} src={constant.image_host + this.props.topic.userAvatar} alt=''/>
+                                            :
+                                            <img className={style.headerLeftImage} src='http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/28/h/28' alt=''/>
+
+                                    }
+                                </div>
+                                <div className={style.headerCenter}>
+                                    <p className={style.headerCenterName}>{this.props.topic.userNickName}</p>
+                                    <p className={style.headerCenterTime}>{moment(this.props.topic.systemCreateTime).fromNow()}</p>
+                                </div>
+                                <div className={style.headerRight}></div>
                             </div>
-                            <div className={style.footerInfoCountBookmark}>
-                                <img className={style.footerInfoCountBookmarkIcon} src={true ? require('../../image/bookmark.png') : require('../../image/bookmark-acitve.png')} alt=''/>
-                                <span className={style.footerInfoCountBookmarkNumber}>10</span>
+                            <Link to="/topic/detail/:topicId" className={style.content}>
+                                {
+                                    this.props.topic.topicMediaList && this.props.topic.topicMediaList.length > 0 ?
+                                        this.props.topic.topicMediaList.map(
+                                            (topicMedia, index) => <img className={style.contentImage} src={constant.image_host + topicMedia.fileOriginalPath} alt='' key={index}/>
+                                        )
+                                        :
+                                        null
+                                }
+                            </Link>
+                            <div className={style.footer}>
+                                <div className={classNames(style.footerText, baseStyle.bottomLine)}>
+                                    {this.props.topic.topicSummary}
+                                </div>
+                                <div className={style.footerInfo}>
+                                    <div className={style.footerInfoCount}>
+                                        <div className={style.footerInfoCountLike}>
+                                            <img className={style.footerInfoCountLikeIcon} src={this.props.topic.topicUserIsLike ? require('../../image/like.png') : require('../../image/like-active.png')} alt=''/>
+                                            <span className={style.footerInfoCountLikeNumber}>{this.props.topic.topicCountLike}</span>
+                                        </div>
+                                        <div className={style.footerInfoCountBookmark}>
+                                            <img className={style.footerInfoCountBookmarkIcon} src={this.props.topic.topicUserIsBookmark ? require('../../image/bookmark.png') : require('../../image/bookmark-acitve.png')} alt=''/>
+                                            <span className={style.footerInfoCountBookmarkNumber}>{this.props.topic.topicCountBookmark}</span>
+                                        </div>
+                                        <div className={style.footerInfoCountComment}>
+                                            <img className={style.footerInfoCountCommentIcon} src={require('../../image/comment.png')} alt=''/>
+                                            <span className={style.footerInfoCountCommentNumber}>{this.props.topic.topicCountComment}</span>
+                                        </div>
+                                    </div>
+                                    <span className={style.footerInfoFrom}>同步到</span>
+                                    {
+                                        this.props.topic.topicForumList && this.props.topic.topicForumList.length > 0 ?
+                                            this.props.topic.topicForumList.map(
+                                                (forum, index) => <span className={style.footerInfoTag} key={index}>{forum.forumName}</span>
+                                            )
+                                            :
+                                            null
+                                    }
+                                </div>
                             </div>
-                            <div className={style.footerInfoCountComment}>
-                                <img className={style.footerInfoCountCommentIcon} src={require('../../image/comment.png')} alt=''/>
-                                <span className={style.footerInfoCountCommentNumber}>10</span>
-                            </div>
+                            <div className={style.line}></div>
                         </div>
-                        <span className={style.footerInfoFrom}>同步到</span>
-                        <span className={style.footerInfoTag}>大爱猫咪控</span>
-                        <span className={style.footerInfoTag}>大爱猫咪控</span>
-                        <span className={style.footerInfoTag}>大爱猫咪控</span>
-                        <span className={style.footerInfoTag}>大爱猫咪控</span>
-                        <span className={style.footerInfoTag}>大爱猫咪控</span>
-                        <span className={style.footerInfoTag}>大爱猫咪控</span>
-                        <span className={style.footerInfoTag}>大爱猫咪控</span>
-                        <span className={style.footerInfoTag}>大爱猫咪控</span>
-                        <span className={style.footerInfoTag}>大爱猫咪控</span>
-                        <span className={style.footerInfoTag}>大爱猫咪控</span>
-                        <span className={style.footerInfoTag}>大爱猫咪控</span>
-                        <span className={style.footerInfoTag}>大爱猫咪控</span>
-                    </div>
-                </div>
-                <div className={style.line}></div>
+                        :
+                        null
+                }
+
             </div>
         );
     }
 }
 
-Index.propTypes = {};
+Index.propTypes = {
+    topic: PropTypes.object.isRequired
+};
 
 export default Index;
