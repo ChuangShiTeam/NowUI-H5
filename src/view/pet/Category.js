5a737b374a172a7021caeb56c6d0355e95bf2746
shawn <xx090304@163.com>
2018-01-26 14:24:45 +0800

    宠物分类修改

--- src/view/pet/Category.js
+++ src/view/pet/Category.js
@@ -2,74 +2,44 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {createForm} from "rc-form";
 import {Link} from 'react-router';
-import Notification from 'rc-notification';
-import classNames from 'classnames';
 import util from '../../common/util';
 import http from '../../common/http';
-import ImageUpload from '../../component/upload/ImageUpload';
 import style from './Category.scss';
 import baseStyle from '../../css/Base.scss';
-import petCategory from "../../store/petCategory";
 
 class Category extends Component {
     constructor(props) {
         super(props);
-
         this.state = {
             isLoad: false,
+            categorys:[]
         }
     }
 
     componentDidMount() {
         util.setTitle('wawipet哇咿宠');
         util.hancleComponentDidMount();
-
         http.request({
             url: '/wawi/pet/category/mobile/v1/list',
             data: {},
             success: function (data) {
-                console.log(data);
+                this.setState({
+                    categorys:data
+                })
             }.bind(this),
             complete: function () {
+
             }
         });
-
-
-        if (this.props.petCategory.categoryList && this.props.petCategory.categoryList.length === 0) {
-
-            // 本地测试静态数据
-            this.props.dispatch({
-                type: 'petCategory',
-                data: {
-                    categoryList:[
-                        {
-                            listItemLeftIcon:"http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/1000/h/1000/q/80",
-                            listItemLeftUrl:"#",
-                            listItemCenterName:"猫类"
-                        },{
-                            listItemLeftIcon:"http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/1000/h/1000/q/80",
-                            listItemLeftUrl:"#",
-                            listItemCenterName:"狗类"
-                        },{
-                            listItemLeftIcon:"http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/1000/h/1000/q/80",
-                            listItemLeftUrl:"#",
-                            listItemCenterName:"鸟类"
-                        }
-                    ]
-                }
-            });
-        }
     }
 
     componentDidUpdate() {
         util.hancleComponentDidUpdate();
     }
 
-    componentWillUnmount() {
-
-    }
-
     render() {
+
+        const {categorys} = this.state;
 
         return (
             <div className={style.page}>
@@ -77,25 +47,27 @@ class Category extends Component {
                     <div className={baseStyle.bottomLine}>宝贝种类</div>
                 </div>
                 <div className={style.content}>
-                   <ul>
-                       {
-                           this.props.petCategory.categoryList.map((categoryList, index) =>
-                               <li key={index} className={baseStyle.bottomLine}>
-                                   <div className={style.listItemLeftIcon}>
-                                       <img src={categoryList.listItemLeftIcon} alt=''/>
-                                   </div>
-                                   <div className={style.listItemCenterName}>
-                                       <Link to={categoryList.listItemLeftUrl}>
-                                           {categoryList.listItemCenterName}
-                                       </Link>
-                                   </div>
-                                   <div className={style.listItemRightArrow}>
-                                       <span className={baseStyle.rightArrow}></span>
-                                   </div>
-                               </li>
-                           )
+                    <ul>
+                        {
+                            categorys.map((categoryList, index) =>
+                                <li key={index} className={baseStyle.bottomLine}>
+                                    <div className={style.listItemLeftIcon}>
+                                        <img src={categoryList.petCategoryImage} alt=''/>
+                                    </div>
+                                    <div className={style.listItemCenterName}>
+                                        <Link to={{
+                                            pathname: `pet/Hotcat/${categoryList.petCategoryId}`
+                                        }}>
+                                            {categoryList.petCategoryName}
+                                        </Link>
+                                    </div>
+                                    <div className={style.listItemRightArrow}>
+                                        <span className={baseStyle.rightArrow}></span>
+                                    </div>
+                                </li>
+                            )
                         }
-                   </ul>
+                    </ul>
                 </div>
             </div>
         );
--- src/view/pet/Hotcat.js
+++ src/view/pet/Hotcat.js
@@ -1,47 +1,89 @@
 import  React, {Component} from 'react';
 import {connect} from 'react-redux';
 import util from '../../common/util';
-import {Link} from 'react-router';
 import  style from './Hotcat.scss';
-import baseStyle from '../../css/Base.scss';
-import classNames from 'classnames';
+import http from '../../common/http';
+import Notification from 'rc-notification';
+import {Link} from 'react-router';
+let notification = null;
+Notification.newInstance({}, (n) => notification = n);
 
 class Hotcat extends Component {
+
     constructor(props) {
         super(props);
+        this.state =
+            {
+                petCategorys: []
+            }
     }
+
     componentDidMount() {
         util.setTitle('wawipet哇咿宠');
         util.hancleComponentDidMount();
+        this.getChildCategoryList(this.props.params.categoryId);
     }
 
-    componentDidUpdate(){
+    getChildCategoryList(categoryId) {
+        http.request({
+            url: '/wawi/pet/category/mobile/v1/child/list',
+            data: {
+                petCategoryId: categoryId
+            },
+            success: function (data) {
+                if (data) {
+                    this.setState({
+                        petCategorys: data
+                    });
+                } else {
+                    notification.notice({
+                        content: '暂无数据'
+                    });
+                }
+            }.bind(this),
+            complete: function () {
+            }
+        });
+    }
+
+    componentDidUpdate() {
         util.hancleComponentDidUpdate();
     }
 
-    componentWillUnmount(){
+    componentWillUnmount() {
+
     }
 
     render() {
+        const {petCategorys} = this.state;
+
         return (
             <div className={style.page} style={{minHeight: document.documentElement.clientHeight}}>
-                <div className={style.header}> 
-                   <span className={style.tittle}>热门品种</span>
-                    <span className={style.allKind}>查看全部</span>
+                <div className={style.header}>
+                    <span className={style.tittle}>热门品种</span>
+
+                    <Link to={{
+                        pathname: `pet/Category`
+                    }}>
+                        <span className={style.allKind}> 查看全部 </span>
+                    </Link>
+
                 </div>
                 <div className={style.list}>
-                    <div className={style.listItem}>
-                        <img src="http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?listView/1/w/56/h/56" alt=""/>
-                        <span>苏格兰折耳猫</span>
-                    </div>
-
+                    {
+                        petCategorys.map((category, index) =>
+                            <div key={index} className={style.listItem}>
+                                <img src={category.petCategoryImage.filePath} alt={category.petCategoryName}/>
+                                <span>{category.petCategoryName}
+                                </span>
+                            </div>
+                        )
+                    }
                 </div>
             </div>
         )
 
     }
-
 }
-
 
 export default connect(() => ({}))(Hotcat);
